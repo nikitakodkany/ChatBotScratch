5b48c143e0a11523ff454042d9a8c97cc0b1c7ed
nikitakodkany <nikitahkodkany@gmail.com>
2020-06-15 13:05:39 +0530

    new

--- chatbot.py
+++ chatbot.py
@@ -2,3 +2,17 @@ import nltk
 import numpy as np
 import random
 import string
+
+#read corpus.txt
+f = open('chatbot.txt', 'r', errors = 'ignore')
+
+raw = f.read()
+#converts to lower case
+raw = raw.lower()
+
+#convert entire corpus into list of sentences and words
+sent_tokens = nltk.sent_tokenize(raw)
+word_tokens = nltk.word_tokenize(raw)
+
+print(sent_tokens[:2])
+print(word_tokens[:2])
